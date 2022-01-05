36196317c93374941e254631f771b95f52b7cbeb
Diego Caballero <dcaballero@salesforce.com>
2022-01-05 17:24:55 -0300

    [DonationHistoryFilter] ComboBox populates with database values

--- force-app/main/default/lwc/donationHistoryFilter/donationHistoryFilter.js
+++ force-app/main/default/lwc/donationHistoryFilter/donationHistoryFilter.js
@@ -1,11 +1,45 @@
-import { LightningElement } from 'lwc';
-
+import { api, LightningElement, track } from 'lwc';
+import getYearsWithDonation from '@salesforce/apex/DonationHistoryController.getYearsWithDonations'
 export default class DonationHistoryFilter extends LightningElement {
     value = 'lifetime';
     
-    get options() {
-        return [{label : 'Lifetime', value : 'lifetime'}, 
-                {label: '2020', value: '2020'}];
+    @track
+    options = [];
+
+    @api
+    contactId
+    connectedCallback() {
+
+        getYearsWithDonation({contactId : this.contactId})
+        .then(
+            result => {
+                this.options = this.generateOptionsFromYearsList(this.generateYearList(result));
+                console.info(this.options);
+            }).catch(error => {
+        
+                console.info(error)}
+                );
+    }
+
+    generateOptionsFromYearsList(yearList){
+
+        let toReturn =[ {label: 'Lifetime', value: 'lifetime'}];
+        yearList.forEach( year => {
+            toReturn.push({label: '' + year, value: '' + year});
+        });
+        return toReturn;
+    } 
+
+    generateYearList(result){
+        let toReturn =[];
+        for (let i = result[0]; i >= result[result.length - 1]; i--) {
+            toReturn.push(i);
+        }
+        return toReturn;
 
     }
+    handleOnchange(e) {
+        this.value = e.detail.value;
+    }
+
 }
